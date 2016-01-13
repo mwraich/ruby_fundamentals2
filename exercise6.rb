9b1c55b7fd50df798bb276853035b34ee2a7370f
mwraich <mwraich28@gmail.com>
2016-01-13 15:14:19 -0500

    Completed question 1-6, exercise completed

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,28 @@
+
+
+def list(list)
+  puts "Grocery List"
+  list.each {|item| puts "* #{item}"}
+end
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon", "avocado"]
 
-grocery_list.each {|item| puts "* #{item}"}
-
-# def list(item)
-#   puts "*#{item}"
-# end
-#
-# list(grocery_list)
+list(grocery_list)
+#1
+grocery_list << "rice"
+list(grocery_list)
+#2
+puts "There are #{grocery_list.length} items in the list"
+#3
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else puts "You don't need to pick up bananas today"
+end
+#4
+puts "The second item on the list is #{grocery_list[1]}"
+#5
+puts (grocery_list.sort!)
+list(grocery_list)
+#6
+grocery_list.delete("salmon")
+list(grocery_list)
