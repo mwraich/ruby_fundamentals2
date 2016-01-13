b861a7b96b538b12f420bd1555b8cb3041808afe
mwraich <mwraich28@gmail.com>
2016-01-13 14:29:00 -0500

    completed item list using .each, working on method way

---
+++ exercise6.rb
@@ -0,0 +1,9 @@
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "avocado"]
+
+grocery_list.each {|item| puts "* #{item}"}
+
+# def list(item)
+#   puts "*#{item}"
+# end
+#
+# list(grocery_list)
